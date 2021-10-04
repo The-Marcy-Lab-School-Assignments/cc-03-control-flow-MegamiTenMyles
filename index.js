b11e04433aed80888961c64ef7036903d64fc2b4
Myles <mylesmatthews2112@gmail.com>
2021-10-04 14:21:39 +0000

    work submitted

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 //1
-function countToTen(){
-  
+function countFromOne(number){
+  for(let i = 1; i <= number; i++){
+    console.log(i)
+  }
 }
-
+//countFromOne(29)
 //2
-function countFromOne(){
+function countEveryOdd(number){
+  for(let i = 1; i <= number; i+=2){
+    console.log(i)
+  }
+  }
+  //countEveryOdd(30)
   
-}
-
 //3
-function countEveryEven(){
+function isNegative(){
   
 }
 
 //4
-function countEveryOdd(){
+function betweenFiveAndTwenty(){
   
 }
 
 //5 
-function countEvens(){
-  
+function isAllLowerCase(string){
+  if (string.toLowerCase() === string){
+    return true
+  } else {
+    return false
+  }
 }
-
+console.log(isAllLowerCase('lol'))
